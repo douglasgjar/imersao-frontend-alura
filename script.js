5a40c45167b784fd734ca180e4532d29ef512be0
Douglas Jardim <douglasgjar@gmail.com>
2024-01-28 18:42:27 +0000

    Ajustes hover, fake api, funções js

---
+++ script.js
@@ -0,0 +1,32 @@
+const searchInput = document.getElementById('header-search-input');
+const resultArtist = document.getElementById('result-artist');
+const resultPlaylist = document.getElementById('result-playlists');
+
+function requestApi(searchTerm) {
+  fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
+    .then((response) => response.json())
+    .then((results) => displayResults(results))
+}
+
+function displayResults(results) {
+  resultPlaylist.classList.add('hidden');
+  const artistName = document.getElementById('artist-name');
+  const artistImage = document.getElementById('artist-img');
+
+  results.forEach(element => {
+    artistName.innerText = element.name;
+    artistImage.src = element.urlImg;  
+  });
+
+  resultArtist.classList.remove('hidden');
+}
+
+document.addEventListener('input', function() {
+  const searchTerm = searchInput.value.toLowerCase();
+  if (searchTerm === '') {
+    resultPlaylist.classList.remove('hidden');
+    resultArtist.classList.add('hidden');
+    return;
+  }
+  requestApi(searchTerm);
+})
